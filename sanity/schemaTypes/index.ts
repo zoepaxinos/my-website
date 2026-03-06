bbd4f571999bb1dd8c94ccf533830abc9ad22847
Zoe Paxinos <zoepaxinos@Mac.gateway>
2026-03-06 11:55:40 +1100

    feat: register all Sanity schemas

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -6,6 +6,22 @@ import { page } from './documents/page';
 // Objects
 import { seo } from './objects/seo';
 
+// Blocks
+import { cta } from './blocks/cta';
+import { testimonial } from './blocks/testimonial';
+import { pricing } from './blocks/pricing';
+import { faq } from './blocks/faq';
+
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [page, seo],
+  types: [
+    // Documents
+    page,
+    // Objects
+    seo,
+    // Blocks
+    cta,
+    testimonial,
+    pricing,
+    faq,
+  ],
 }
